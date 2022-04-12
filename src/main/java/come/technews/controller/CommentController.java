ff0404642c7f50ae2115f7969c2a26b794faab66
Sloan <Sloan.boyce@gmail.com>
2022-04-12 15:28:42 -0400

    added comment controller

---
+++ src/main/java/come/technews/controller/CommentController.java
@@ -0,0 +1,43 @@
+package come.technews.controller;
+
+import come.technews.model.Comment;
+import come.technews.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentController {
+	
+	@Autowired
+	CommentRepository repository;
+	
+	@GetMapping("/api/comments")
+	public List<Comment> getAllComments() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/api/comments/{id}")
+	public Comment getComment(@PathVariable int id) {
+		return repository.getById(id);
+	}
+	
+	@PostMapping("/api/comments")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Comment createComment(@RequestBody Comment comment) {
+		return repository.save(comment);
+	}
+	
+	@PutMapping("/api/comments/{id}")
+	public Comment updateComment(@RequestBody Comment comment) {
+		return repository.save(comment);
+	}
+	
+	@DeleteMapping("/api/comments/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteComment(@PathVariable int id) {
+		repository.deleteById(id);
+	}
+}
--- src/main/java/come/technews/controller/PostController.java
+++ src/main/java/come/technews/controller/PostController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@RestController
 public class PostController {
 	
 	@Autowired
